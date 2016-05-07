42c6cd9fa2b4fb2ab4a0c1c34886bae213f1bbbc
pferrel <pat@occamsmachete.com>
2016-05-07 10:00:00 -0700

    starting to add docs

--- metadata.js
+++ metadata.js
@@ -1,43 +1,38 @@
 DocList = [
-{
-    title: 'Markdown test',
-    template: 'docs_md_test'
-},
-{
-    title: 'Welcome to ActionML',
-    template: 'welcome_to_actionml',
-    sections: [
-      'Placeholder title',
-    ]
-},
-{
-    title: 'Integrating with your app',
-    template: 'integrating_with_your_app',
-    sections: [
-      'App integration overview',
-      'List of SDKs',
-      'Event server overview',
-    ]
-},
-{
-    title: 'Deploying and engine',
-    template: 'deploying_and_engine',
-    sections: [
-      'Placeholder title',
-    ]
-},
-{
-    title: 'Choosing and algorithm(s)',
-    template: 'choosing_and_algorithms',
-    sections: [
-        'Placeholder title',
-    ]
-},
-{
-    title: 'Demo tutorials',
-    template: 'demo_tutorials',
-    sections: [
-        'Placeholder title',
-    ]
-},
+    {
+        title: 'Welcome to ActionML',
+        template: 'welcome_to_actionml',
+    },
+    {
+        title: 'PredictionIO',
+        template: 'predictionio_by_actionml',
+        sections: [
+          {title:'Install', template: 'install'}
+        ]
+    },
+    {
+        title: 'Deploying and engine',
+        template: 'deploying_and_engine',
+        sections: [
+          'Placeholder title',
+        ]
+    },
+    {
+        title: 'Choosing and algorithm(s)',
+        template: 'choosing_and_algorithms',
+        sections: [
+            'Placeholder title',
+        ]
+    },
+    {
+        title: 'Demo tutorials',
+        template: 'demo_tutorials',
+        sections: [
+            'Placeholder title',
+        ]
+    },
+    {
+        title: 'Markdown test',
+        template: 'docs_md_test'
+    },
 ];
