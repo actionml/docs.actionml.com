3921418b57555c3e68ec83cc3f84972e736f07b8
pote <pote@tardis.com.uy>
2016-05-09 19:39:48 +0200

    Remove old test sections

--- metadata.js
+++ metadata.js
@@ -2,6 +2,7 @@ DocList = [
     {
         title: 'Welcome to ActionML',
         template: 'welcome_to_actionml',
+        sections: []
     },
     {
         title: 'PredictionIO',
@@ -9,7 +10,7 @@ DocList = [
         sections: [
             {
                 title: 'Installing',
-                template: 'install'
+                template: "install"
             }
         ]
     },
@@ -17,34 +18,23 @@ DocList = [
         title: 'Deploying and engine',
         template: 'deploying_and_engine',
         sections: [
-            {
-                title: 'Placeholder title',
-                template: 'placeholder_title'
-            }
         ]
     },
     {
         title: 'Choosing and algorithm(s)',
         template: 'choosing_and_algorithms',
         sections: [
-            {
-                title: 'Placeholder title',
-                template: 'placeholder_title'
-            }
         ]
     },
     {
         title: 'Demo tutorials',
         template: 'demo_tutorials',
         sections: [
-            {
-                title: 'Placeholder title',
-                template: 'placeholder_title'
-            }
         ]
     },
     {
         title: 'Markdown test',
-        template: 'docs_md_test'
+        template: 'docs_md_test',
+        sections: []
     },
 ];
