eadea224ce6b8cc325007e26f736faf33aa9d534
pferrel <pat@occamsmachete.com>
2016-05-09 11:08:47 -0700

    merge

--- metadata.js
+++ metadata.js
@@ -10,31 +10,8 @@ DocList = [
         sections: [
             {
                 title: 'Installing',
-<<<<<<< HEAD
                 template: 'install'
-            }        
-=======
-                template: "install"
             }
-        ]
-    },
-    {
-        title: 'Deploying and engine',
-        template: 'deploying_and_engine',
-        sections: [
-        ]
-    },
-    {
-        title: 'Choosing and algorithm(s)',
-        template: 'choosing_and_algorithms',
-        sections: [
-        ]
-    },
-    {
-        title: 'Demo tutorials',
-        template: 'demo_tutorials',
-        sections: [
->>>>>>> b279bef667e28cf3e760a821c326faa0c9f61fe7
         ]
     },
     {
